7e1a133c4f504546cec70d97ae67fb79342e60e5
Aftab@khan <affu123affu>
2023-03-05 15:18:34 +0530

    practice day

---
+++ src/Greet.jsx
@@ -0,0 +1,32 @@
+
+function Greet() {
+    let Dates = new Date();
+    let GetHours = Dates.getHours()
+    let greeting = ""
+    let Colors = {}
+    if (GetHours >= 1 && GetHours <= 12) {
+        greeting = 'Good Morning';
+        Colors.color = "green"
+    }
+    else if (GetHours <= 13 && GetHours > 18) {
+        greeting = "Good Aternoon"
+        Colors.color = "red"
+    }
+    else if (GetHours <= 18 && GetHours >= 20) {
+        greeting = "Good Evening"
+        Colors.color = "gray"
+    }
+    else {
+        greeting = "Good Night"
+        Colors.color = "black"
+    }
+    return (
+        <>
+            <div className='head'>
+                <h1>Hello Sir:<span style={Colors}>{greeting}</span></h1>
+            </div>
+        </>
+    )
+}
+
+export default Greet;
